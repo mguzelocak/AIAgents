89c0806c0f1b216066e0373915be877e477806f1
mguzelocak <mrtcnguzelocak@gmail.com>
2025-04-19 12:20:11 -0600

    two agents who are controlled by an routing agent is created

--- src/basic.py
+++ src/basic.py
@@ -5,10 +5,26 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# pytohn agent
-agent = Agent(
-  name="python-agent",
-  instructions="You are an expert Python programming language"
+# product agent
+productAsistant = Agent(
+  name="Beyorganik Gida Urun Tavsiyeleri Asistani",
+  instructions="Sen cok iyi bir Beyorganik Gida urun tavsiyeleri yapan bir asistansin.",
+  handoff_description="Senin bir Beyorganik Gida urun tavsiyeleri yapan bir asistansin. Verilen querye gore uygun urunleri tavsiye ediyorsun.",
 )
-response = Runner.run_sync(agent, "How to make for loop in Python?")
+
+# Create a new agent
+pythonAgent = Agent(
+  name="Python Asistani",
+  instructions="Sen cok iyi bir Python programlama dili asistanisin.",
+  handoff_description="Senin bir Python programlama dili asistanisin. Verilen querye gore uygun python kodu yaziyorsun.",
+)
+
+#routing agent
+routingAgent = Agent(
+  name="Routing Asistani",
+  instructions="Senin bir handoff asistanisin. Verilen querye gore uygun agenti secip ona yonlendiriyorsun.",
+  handoffs=[productAsistant, pythonAgent],
+)
+
+response = Runner.run_sync(routingAgent, "while loop nasil kullanilir?")
 print(f"Instruction: {response.input}\n{response.final_output} ")
